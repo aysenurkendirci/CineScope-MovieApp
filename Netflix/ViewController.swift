658369985a158f4d846f7d0067f222431a7a040d
aysenurkendirci <aysenurkendirciss@gmail.com>
2025-08-19 11:38:07 +0300

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Ayşe Nur Kendirci on 19.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
